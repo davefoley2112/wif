8c650d8feff6567b3a58c83cfea8d75aa35ec4d6
Dave Foley <dfoley@theatomgroup.com>
2013-05-13 10:15:22 -0400

    Added some comments!

--- Chapter 1/Recipe 1/WindowsIdentityToClaimsConsole/WindowsIdentityToClaimsConsole/Program.cs
+++ Chapter 1/Recipe 1/WindowsIdentityToClaimsConsole/WindowsIdentityToClaimsConsole/Program.cs
@@ -11,6 +11,21 @@ namespace WindowsIdentityToClaimsConsole
     {
         static void Main(string[] args)
         {
+            //
+            // A claim is used to identify a user or provide access to a particular resource requested by the user. 
+            // There are three properties exposed by the Claim class:
+            //
+            // ClaimType: It identifies the type of claim. In our example, Sid (security identifier) and Name are the 
+            //            two claim types displayed in the console window. 
+            //
+            // Resource: It identifies the resource associated with the claim.
+            //
+            // Right: It is a URI representing the Identity or PossessProperty right associated with the claim. 
+            //        PossessProperty determines whether the user has the access to Resource.
+            //
+            // Both the Claim and the ClaimSet classes are serialization-friendly, 
+            // which allows them to be transmitted over service boundaries.
+            //
             using (WindowsClaimSet claims = new WindowsClaimSet(WindowsIdentity.GetCurrent()))
             {
                 foreach (var claim in claims)
@@ -18,7 +33,7 @@ namespace WindowsIdentityToClaimsConsole
                     Console.WriteLine(string.Format("Claim Type: {0}", claim.ClaimType));
                     Console.WriteLine(string.Format("Resource: {0}", claim.Resource.ToString()));
                     Console.WriteLine(string.Format("Right: {0}", claim.Right));
-                    Console.WriteLine("**********************************************");
+                    Console.WriteLine("\n");
                 }
             }
             
