dfae8a93f24937b11c8658f8e6d612bd77d64445
Dave Foley <dfoley@theatomgroup.com>
2013-05-13 10:28:22 -0400

    Added types for Class Diagram

--- Chapter 1/Recipe 1/WindowsIdentityToClaimsConsole/WindowsIdentityToClaimsConsole/Program.cs
+++ Chapter 1/Recipe 1/WindowsIdentityToClaimsConsole/WindowsIdentityToClaimsConsole/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.IdentityModel.Claims;
 using System.Security.Principal;
+using System.Security.Cryptography.X509Certificates;
+using System.IdentityModel.Policy;
 
 namespace WindowsIdentityToClaimsConsole
 {
@@ -30,7 +32,9 @@ namespace WindowsIdentityToClaimsConsole
             {
                 foreach (var claim in claims)
                 {
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine(string.Format("Claim Type: {0}", claim.ClaimType));
+                    Console.ForegroundColor = ConsoleColor.White;
                     Console.WriteLine(string.Format("Resource: {0}", claim.Resource.ToString()));
                     Console.WriteLine(string.Format("Right: {0}", claim.Right));
                     Console.WriteLine("\n");
@@ -38,6 +42,31 @@ namespace WindowsIdentityToClaimsConsole
             }
             
             Console.ReadLine();
+        }
+
+        static void DummyMethodForClassDiagram()
+        {
+            //
+            // There's more:
+            //
+            // In addition to WindowsClaimSet, the System.IdentityModel.Claims namespace provides a DefaultClaimSet 
+            // class that allows you to create your implementation of claims, and a X509CertificateClaimSet class 
+            // to abstract claims from an X.509 certificate.
+            //
+            var a1 = new DefaultClaimSet();
+            var a2 = new X509CertificateClaimSet(new X509Certificate2());
+
+            //
+            // Authorization context:
+            //
+            // The System.IdentityModel.Policy namespace exposes a AuthorizationContext class that can be used to 
+            // evaluate the authorization policies in a sent message. The AuthorizationContext class has a ClaimSet 
+            // property that allows a service to retrieve all the claims associated with the security token in the 
+            // sent message. 
+            //
+            // You can learn more with an example in the MSDN documentation at http://msdn.microsoft.com/en-us/library/system.identitymodel.policy.authorizationcontext.claimsets.aspx.
+            //
+            AuthorizationContext a3;
         }
     }
 }
