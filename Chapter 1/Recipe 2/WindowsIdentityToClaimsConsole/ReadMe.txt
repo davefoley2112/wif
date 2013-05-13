290f56121e0964f3f0426330f2e094f569104a56
Dave Foley <dfoley@theatomgroup.com>
2013-05-13 11:45:08 -0400

    Example 2 - SAML

--- Chapter 1/Recipe 2/WindowsIdentityToClaimsConsole/ReadMe.txt
+++ Chapter 1/Recipe 2/WindowsIdentityToClaimsConsole/ReadMe.txt
@@ -1,6 +1,28 @@
 1. Run Visual Studio in Administrative Mode
+
 2. Make sure you are connected to the domain if your credentials are provided by Active Directory.
+
 3. Make sure you have appropriate rights to create a file in C Drive. Else change the path in code to point to a location where you have the rights.
+
 4. Install the certificate by running Visual Studio Tools Command prompt in Administrative Mode and using the following command:
 makecert -r -pe -n "CN=SamlTokenSigningCertificate" -b 01/01/2011 -e 01/01/2013 -sky exchange -ss my
+
+	-r		Creates a self-signed certificate.
+
+	-pe		Marks the generated private key as exportable. This allows the private key to be included in the certificate.
+
+	-n		Specifies the subject's certificate name. This name must conform to the X.500 standard. The simplest method is to specify the name in double quotes, preceded by CN=; for example, -n "CN=myName".
+
+	-b		Specifies the start of the validity period. Defaults to the current date.
+
+	-e		Specifies the end of the validity period. Defaults to 12/31/2039 11:59:59 GMT.
+
+	-sky	Specifies the subject's key type, which must be one of the following: 
+				* signature (which indicates that the key is used for a digital signature)
+				* exchange (which indicates that the key is used for key encryption and key exchange)
+				* an integer that represents a provider type. By default, you can pass 1 for an exchange key or 2 for a signature key.	
+
+	-ss		Specifies the subject's certificate store name that stores the output certificate.
+			For an example that displays the names of all standard certificate stores found on a local system, see the X509Store.Name property.
+
 5. Compile and Run the application.
--- Chapter 1/Recipe 2/WindowsIdentityToClaimsConsole/WindowsIdentityToClaimsConsole/Program.cs
+++ Chapter 1/Recipe 2/WindowsIdentityToClaimsConsole/WindowsIdentityToClaimsConsole/Program.cs
@@ -50,6 +50,7 @@ namespace WindowsIdentityToClaimsConsole
 
             assertion.Statements.Add(statement);
             SignSamlAssertion(assertion);
+
             return assertion;
         }
 
@@ -65,12 +66,20 @@ namespace WindowsIdentityToClaimsConsole
             }
         }
 
+        //
+        // The SAML token needs to be digitally signed using a certificate to validate the authenticity 
+        // of the issuer. In our solution, we are signing the assertion using a self-issued X.509 certificate. 
+        // The SamlAssertion class exposes a SigningCredentials property that is used to digitally sign the 
+        // SAML assertion:
+        //
         private static void SignSamlAssertion(SamlAssertion assertion)
         {
             X509Certificate2 certificate2 = GetCertificateFromStore(StoreLocation.CurrentUser, DateTime.Now, "CN=SamlTokenSigningCertificate");
+
             if (certificate2 != null)
             {
                 X509AsymmetricSecurityKey securityKey = new X509AsymmetricSecurityKey(certificate2);
+
                 assertion.SigningCredentials = new SigningCredentials(
                     securityKey,
                     SecurityAlgorithms.RsaSha1Signature,
@@ -82,13 +91,16 @@ namespace WindowsIdentityToClaimsConsole
         private static X509Certificate2 GetCertificateFromStore(StoreLocation location, DateTime timeValid, string subjectDistinguishedName)
         {
             X509Store store = new X509Store(location);
+
             try
             {
                 store.Open(OpenFlags.ReadOnly);
                 X509Certificate2Collection validCertificates = store.Certificates.Find(X509FindType.FindByTimeValid, timeValid, false);
                 X509Certificate2Collection signingCertificate = validCertificates.Find(X509FindType.FindBySubjectDistinguishedName, subjectDistinguishedName, false);
+
                 if (signingCertificate.Count == 0)
                     return null;
+
                 return signingCertificate[0];
             }
             finally
@@ -97,9 +109,15 @@ namespace WindowsIdentityToClaimsConsole
             }
         }
 
+        //
+        // The System.ServiceModel.Security namespace has a WSSecurityTokenSerializer class that can be 
+        // used to serialize and deserialize the security tokens conforming to the WS-Security and the 
+        // WS-Trust specifications. In our solution, we are using a XmlWriter class to persist the token 
+        // into the filesystem:
+        //
         private static void SerializeSamlTokenToFile(SamlSecurityToken token)
         {
-            using (XmlWriter writer = XmlWriter.Create(@"c:\saml.xml"))
+            using (XmlWriter writer = XmlWriter.Create(@"c:\github\wif\saml.xml"))
             {
                 try
                 {
